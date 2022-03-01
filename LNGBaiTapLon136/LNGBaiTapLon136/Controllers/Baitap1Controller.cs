d65f76d22692a4480044f602ed46d76e0080c306
min <min@DESKTOP-T2NAVI1>
2022-03-01 08:52:04 +0700

    Bai1

---
+++ LNGBaiTapLon136/LNGBaiTapLon136/Controllers/Baitap1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LNGBaiTapLon136.Controllers
+{
+    public class Baitap1Controller : Controller
+    {
+        // GET: Baitap1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LNGBaiThucHanh136/LNGBaiThucHanh136/Controllers/Bai1Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LNGBaiThucHanh136.Controllers
+{
+    public class Bai1Controller : Controller
+    {
+        // GET: Bai1
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Bai1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Bai1(string Masinhvien, string Hoten, string Lop)
+        {
+            ViewBag.Thongtin = Masinhvien + "" + Hoten + "" + Lop;
+            return View();
+        }
+    }
+}
